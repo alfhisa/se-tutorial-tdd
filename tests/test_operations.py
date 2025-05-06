73b1f4336763a54961b1be3d587b9c77e9747be3
uikayo <rachellecelinaa@gmail.com>
2025-05-06 16:17:51 +0700

    Ini sudah kami tambahkan pak

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -1,5 +1,5 @@
 import unittest
-from calculator.operations import add, subtract, multiply, divide
+from calculator.function import add, sub, div, mul
 
 class TestCalculator(unittest.TestCase):
     def test_add(self):
@@ -7,17 +7,20 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(add(-1, 1), 0)
         self.assertEqual(add(0, 0), 0)
 
-    def test_subtract(self):
-        # TODO: add assertions for subtract
-        pass
+    def test_sub(self):
+        self.assertEqual(sub(2, 3), -1)
+        self.assertEqual(sub(-1, 1), -2)
+        self.assertEqual(sub(0, 0), 0)
 
-    def test_multiply(self):
-        # TODO: add assertions for multiply
-        pass
+    def test_div(self):
+        self.assertEqual(div(4, 2), 2)
+        self.assertEqual(div(10, 5), 2)
+        self.assertEqual(div(1, 1), 1)
 
-    def test_divide(self):
-        # TODO: add assertions for divide
-        pass
+    def test_mul(self):
+        self.assertEqual(mul(2, 3), 6)
+        self.assertEqual(mul(-1, 1), -1)
+        self.assertEqual(mul(0, 0), 0)
 
 if __name__ == '__main__':
     unittest.main()
