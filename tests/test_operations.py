b41b554cab51aed9a8b1e44fda6e195bd035bc10
Raihan-Khalis <raihankhalis0804@gmail.com>
2025-05-08 12:48:55 +0700

    hi

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -3,21 +3,24 @@ from calculator.operations import add, subtract, multiply, divide
 
 class TestCalculator(unittest.TestCase):
     def test_add(self):
-        self.assertEqual(add(2, 3), 5)
-        self.assertEqual(add(-1, 1), 0)
+        self.assertEqual(add(3, 5), 8)
+        self.assertEqual(add(1, 0), 1)
         self.assertEqual(add(0, 0), 0)
 
     def test_subtract(self):
-        # TODO: add assertions for subtract
-        pass
+        self.assertEqual(subtract(3, 5), -2)
+        self.assertEqual(subtract(-1, 1), -2)
+        self.assertEqual(subtract(0, 0), 0)
 
     def test_multiply(self):
-        # TODO: add assertions for multiply
-        pass
+        self.assertEqual(multiply(2, 3), 6)
+        self.assertEqual(multiply(-1, 1), -1)
+        self.assertEqual(multiply(0, 0), 0)
 
     def test_divide(self):
-        # TODO: add assertions for divide
-        pass
+        self.assertEqual(divide(6, 3), 2)
+        self.assertEqual(divide(-5, 1), -5)
+        self.assertEqual(divide(3, 3), 1)
 
 if __name__ == '__main__':
     unittest.main()
