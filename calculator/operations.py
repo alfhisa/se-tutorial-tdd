cf1668e44e95ec08c22220adc5409d956308d383
Andrew Sutiono <sutionoandrew@gmail.com>
2025-05-08 12:03:03 +0700

    removed "pass"

--- calculator/operations.py
+++ calculator/operations.py
@@ -4,16 +4,13 @@ def add(a, b):
 
 def subtract(a, b):
     return a - b
-    pass
 
 
 def multiply(a, b):
     return a * b
-    pass
 
 
 def divide(a, b):
     if(b == 0):
         raise ValueError("Cannot divide by zero")
     return a / b
-    pass
