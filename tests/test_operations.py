17f006cc68549df3c1d65f272faa656d2bcd28d2
Ervan <ervan.gkbi@gmail.com>
2025-05-08 12:55:34 +0700

    Perubahan pada file TDD

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -4,20 +4,9 @@ from calculator.operations import add, subtract, multiply, divide
 class TestCalculator(unittest.TestCase):
     def test_add(self):
         self.assertEqual(add(2, 3), 5)
-        self.assertEqual(add(-1, 1), 0)
-        self.assertEqual(add(0, 0), 0)
-
-    def test_subtract(self):
-        # TODO: add assertions for subtract
-        pass
-
-    def test_multiply(self):
-        # TODO: add assertions for multiply
-        pass
-
-    def test_divide(self):
-        # TODO: add assertions for divide
-        pass
+        self.assertEqual(subtract(4, 1), 3)
+        self.assertEqual(multiply(2, 1), 2)
+        self.assertEqual(divide(2, 0), 'undefined')
 
 if __name__ == '__main__':
     unittest.main()
